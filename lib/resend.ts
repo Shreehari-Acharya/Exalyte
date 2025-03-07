ad5aca3a63a376ecffaf509461415793011a2d6a
Shreehari Acharya <shreehari.acharya.06@gmail.com>
2025-03-07 11:55:04 +0530

    refactor: change the env variable name to AUTH_RESEND_KEY

--- lib/resend.ts
+++ lib/resend.ts
@@ -2,6 +2,6 @@ import { Resend } from "resend";
 
 const globalForResend = global as unknown as { resend?: Resend };
 
-export const resend = globalForResend.resend ?? new Resend(process.env.RESEND_API_KEY);
+export const resend = globalForResend.resend ?? new Resend(process.env.AUTH_RESEND_KEY);
 
 if (process.env.NODE_ENV !== "production") globalForResend.resend = resend;
